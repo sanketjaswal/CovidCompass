0a2d01f7889c7dce380c749603bbb4231bca7d76
Sanket Jaswal <sanket.jaswal123@gmail.com>
2023-06-21 20:53:48 +0530

    Updates

---
+++ precache-manifest.146959e6d73a78aecc59e9d11bff5b97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cc9cb64ca56e8af4371",
+    "url": "/covid_tracker/static/css/main.80daab7f.chunk.css"
+  },
+  {
+    "revision": "9cc9cb64ca56e8af4371",
+    "url": "/covid_tracker/static/js/main.9cc9cb64.chunk.js"
+  },
+  {
+    "revision": "2981538dd718ef902032",
+    "url": "/covid_tracker/static/js/1.2981538d.chunk.js"
+  },
+  {
+    "revision": "11a3eb75ba923fb4e350",
+    "url": "/covid_tracker/static/js/2.11a3eb75.chunk.js"
+  },
+  {
+    "revision": "2da2c857cb7e46680e28",
+    "url": "/covid_tracker/static/js/runtime~main.2da2c857.js"
+  },
+  {
+    "revision": "d27b29002508b73d7dae824e6e3e04f8",
+    "url": "/covid_tracker/index.html"
+  }
+];
